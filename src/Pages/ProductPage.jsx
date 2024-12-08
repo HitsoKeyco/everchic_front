805deb9bc0de33611c60bed33f422da1bf04a669
HitsoKeyco <olivosergio09@gmail.com>
2024-12-07 21:30:25 -0500

    t

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,8 +1,7 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import Fetchproducts from "./js/Fetchproducts";
-import { Autocomplete, Backdrop, Box, CircularProgress, Pagination, Stack, Tab, Tabs, TextField, Typography } from "@mui/material";
-import CardProduct from "../Components/CardProduct";
-import { alignProperty } from "@mui/material/styles/cssUtils";
+import { useState, useEffect } from 'react';
+import Fetchproducts from './js/Fetchproducts';
+import { Box, Autocomplete, TextField, Typography, Backdrop, CircularProgress, Pagination, Stack, Tab, Tabs } from '@mui/material';
+import CardProduct from '../Components/CardProduct';
 
 const ProductPage = () => {
     const {
@@ -12,109 +11,95 @@ const ProductPage = () => {
         loading,
         error,
         fetchProducts,
-        fetchCollections,
         fetchCategories,
-        fetchProductsByCollection,
+        fetchCollections,
         fetchProductsByCategory,
+        fetchProductsByCollection,
         fetchProductsByTags,
     } = Fetchproducts();
 
-    const [pagination, setPagination] = useState({
-        total: 0,
-        currentPage: 1, // Siempre comienza en la página 1
-        totalPages: 0,
-    });
-
+    // cantidad de productos por página
     const quantityProducts = 24;
-    const memorizedCategories = useMemo(() => categories, [categories]);
-    const memorizedCollections = useMemo(() => collections, [collections]);
 
-    const [filters, setFilters] = useState({
-        searchTag: "",
-        selectedCategoryId: null,
-        selectedCollectionId: null,
-    });
-
+    // Estados
+    const [pagination, setPagination] = useState({ currentPage: 1, totalPages: 0 });
     const [activeTab, setActiveTab] = useState(0);
+    const [searchTag, setSearchTag] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedCollection, setSelectedCollection] = useState(null);
+    const [debouncedSearchTag, setDebouncedSearchTag] = useState(searchTag);
 
+    // Cargar productos categorías y colecciones solo una vez
     useEffect(() => {
-        fetchCollections();
+        fetchProducts(1, quantityProducts);
         fetchCategories();
-    }, [fetchCollections, fetchCategories]);
+        fetchCollections();
+    }, [fetchCategories, fetchCollections, fetchProducts]);
 
-    // Función para manejar la paginación
-    const goToPage = (event, page) => {
-        const pageNumber = Number(page); // Asegurarse de que sea un número
-        setPagination((prev) => ({ ...prev, currentPage: pageNumber }));
-    };
-
-    // Efecto para actualizar productos según la página y los filtros activos
     useEffect(() => {
-        const { searchTag, selectedCategoryId, selectedCollectionId } = filters;
-
-        // Restablecer la página a 1 cuando los filtros cambian
-        setPagination((prev) => ({ ...prev, currentPage: 1 }));
-
-        // Llamada a la función correspondiente según el filtro activo
-        if (activeTab === 0 && searchTag) {
-            setPagination((prev) => ({ ...prev, currentPage: 1, totalPages: 1 }));
-            fetchProductsByTags(pagination.currentPage, quantityProducts, searchTag);
-        } else if (activeTab === 1 && selectedCategoryId) {
-            setPagination((prev) => ({ ...prev, currentPage: 1, totalPages: 1 }));
-            fetchProductsByCategory(pagination.currentPage, quantityProducts, selectedCategoryId);
-
-        } else if (activeTab === 2 && selectedCollectionId) {
-            // Restablecer la página a 1 cuando los filtros cambian
-            setPagination((prev) => ({ ...prev, currentPage: 1, totalPages: 1 }));
-            fetchProductsByCollection(pagination.currentPage, quantityProducts, selectedCollectionId);
-        } else {
-            fetchProducts(pagination.currentPage, quantityProducts);
-        }
-    }, [pagination.currentPage, filters, activeTab, fetchProducts, fetchProductsByTags, fetchProductsByCategory, fetchProductsByCollection]);
-
-    // Actualiza la paginación según los datos recibidos
-    useEffect(() => {
-        if (products?.total) {
-            // Si no hay productos, ajustamos la paginación para mostrar solo una página
-            if (products.total === 0) {
-                setPagination({
-                    total: 0,
-                    currentPage: 1,
-                    totalPages: 1,
-                });
-            } else {
-                setPagination({
-                    total: Number(products.total),
-                    currentPage: Number(products.currentPage), // Asegurarse de que sea un número
-                    totalPages: Number(products.totalPages),
-                });
-            }
+        // Actualizar la paginación cuando se actualicen los productos
+        if (products?.total && products?.totalPages) {
+            setPagination({ currentPage: products?.currentPage || 1, totalPages: products?.totalPages || 0 });
         }
     }, [products]);
 
-    // Función de debounce
-    const debounce = useCallback((func, delay) => {
-        let debounceTimeout;
-        return (...args) => {
-            clearTimeout(debounceTimeout);
-            debounceTimeout = setTimeout(() => func.apply(this, args), delay);
-        };
-    }, []);
+    // Manejo de cambios en filtros de búsqueda con debounce
+    const handleSearchChange = (event) => {
+        const newSearchTag = event.target.value;
+        setSearchTag(newSearchTag);
+    };
 
-    const handleTabChange = (event, newValue) => {
+    // Aplicar el debounce
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedSearchTag(searchTag);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer); // Limpiar el temporizador anterior si cambia el valor de searchTag
+        };
+    }, [searchTag]);
+
+    useEffect(() => {
+        // Llamada a fetchProductsByTags cuando el searchTag es actualizado por el debounce
+        if (debouncedSearchTag) {
+            updateProducts(1, debouncedSearchTag, selectedCategory, selectedCollection);
+        } else {
+            updateProducts(1, '', selectedCategory, selectedCollection);
+        }
+    }, [debouncedSearchTag, selectedCategory, selectedCollection]);
+
+    const handleCategoryChange = (_, value) => {
+        setSelectedCategory(value?.id || null); // Guardar el ID de la categoría
+        updateProducts(1, searchTag, value?.id, selectedCollection);
+    };
+
+    const handleCollectionChange = (_, value) => {
+        setSelectedCollection(value?.id || null); // Guardar el ID de la colección
+        updateProducts(1, searchTag, selectedCategory, value?.id);
+    };
+
+    const updateProducts = (page, searchTag, category, collection) => {
+        if (searchTag) {
+            fetchProductsByTags(page, quantityProducts, searchTag);
+        } else if (category) {
+            fetchProductsByCategory(page, quantityProducts, category); // Pasar el ID de la categoría
+        } else if (collection) {
+            fetchProductsByCollection(page, quantityProducts, collection); // Pasar el ID de la colección
+        } else {
+            fetchProducts(page, quantityProducts);
+        }
+    };
+
+    // Manejo de cambio de tab (Buscar, Categoría, Colección)
+    const handleTabChange = (_, newValue) => {
         setActiveTab(newValue);
     };
 
-    const handleSearchChange = (event) => {
-        setFilters((prev) => ({ ...prev, searchTag: event.target.value }));
-    };
-
-    const handleCategoryChange = (event, value) => {
-        setFilters((prev) => ({ ...prev, selectedCategoryId: value ? value.id : null }));
-    };
-
-    const handleCollectionChange = (event, value) => {
-        setFilters((prev) => ({ ...prev, selectedCollectionId: value ? value.id : null }));
+    // Función de paginación
+    const handlePageChange = (_, newPage) => {
+        setPagination((prev) => ({ ...prev, currentPage: newPage }));
+        updateProducts(newPage, searchTag, selectedCategory, selectedCollection); // Actualizar productos según la página y filtros
     };
 
     return (
@@ -127,47 +112,49 @@ const ProductPage = () => {
             display: "flex",
             flexDirection: "column",
             '@media (min-width:600px)': { marginTop: '130px' }
-
         }}>
-            <Box style={{ paddingBottom: "15px", maxWidth: "500px" }}>
-                <Tabs value={activeTab} style={{ paddingBottom: "15px" }} onChange={handleTabChange}>
+            <Box sx={{ paddingBottom: '15px', maxWidth: '500px' }}>
+                <Tabs value={activeTab} style={{ paddingBottom: "15px" }} onChange={handleTabChange} variant="fullWidth">
                     <Tab label="Buscar" />
                     <Tab label="Categoría" />
-                    <Tab label="Colecciones" />
+                    <Tab label="Colección" />
                 </Tabs>
 
                 {activeTab === 0 && (
                     <TextField
                         id="filter-search"
-                        style={{ width: "100%" }}
+                        sx={{ marginTop: '15px' }}
                         label="Escriba aquí alguna referencia o tag"
                         variant="outlined"
-                        onChange={debounce(handleSearchChange, 300)}
+                        fullWidth
+                        value={searchTag}
+                        onChange={handleSearchChange}
                     />
                 )}
 
                 {activeTab === 1 && (
                     <Autocomplete
                         id="filter-category"
-                        options={memorizedCategories}
+                        options={categories}
                         getOptionLabel={(option) => option.name || ""}
-                        onChange={debounce(handleCategoryChange, 300)}
-                        renderInput={(params) => <TextField {...params} label="Escriba una categoría o seleccione una" variant="outlined" />}
+                        value={selectedCategory ? categories.find(cat => cat.id === selectedCategory) : null}
+                        onChange={handleCategoryChange}
+                        renderInput={(params) => <TextField {...params} sx={{ marginTop: '15px' }} label="Seleccione una categoría" variant="outlined" />}
                     />
                 )}
 
                 {activeTab === 2 && (
                     <Autocomplete
                         id="filter-collection"
-                        options={memorizedCollections}
+                        options={collections}
                         getOptionLabel={(option) => option.name || ""}
-                        onChange={debounce(handleCollectionChange, 300)}
-                        renderInput={(params) => <TextField {...params} label="Escriba una colección o seleccione una" variant="outlined" />}
+                        value={selectedCollection ? collections.find(coll => coll.id === selectedCollection) : null}
+                        onChange={handleCollectionChange}
+                        renderInput={(params) => <TextField {...params} sx={{ marginTop: '15px' }} label="Seleccione una colección" variant="outlined" />}
                     />
                 )}
             </Box>
 
-            {/* Aquí se renderizan los productos */}
             <Box
                 sx={{
                     marginTop: "10px",
@@ -182,46 +169,34 @@ const ProductPage = () => {
                     },
                 }}
             >
-                {products?.products &&
-                    products.products.map((product) => (
-                        <CardProduct key={product.id} product={product} />
-                    ))}
+                {products?.products?.map((product) => (
+                    <CardProduct key={product.id} product={product} />
+                ))}
             </Box>
 
-
-            <Box style={{ marginTop: "10px", padding: "5px", display: 'grid', gap: '10px' }}>
-                {error && (
-                    <Typography variant="h6" color="error">
-                        ¡Ups! Ha ocurrido un error
-                    </Typography>
-                )}
-
-                {products?.products?.length === 0 && !error && (
-                    <Typography variant="h6" color="textSecondary" textAlign="center">
-                        No hay productos disponible!
-                    </Typography>
-                )}
-            </Box>
-
-            {/* Paginación solo si hay productos */}
-            {pagination.total > 0 && (
-                <Stack spacing={2} direction="row" justifyContent="center" padding={3}>
-                    <Pagination
-                        count={pagination.totalPages}
-                        page={pagination.currentPage}
-                        onChange={goToPage}
-                        variant="outlined"
-                        shape="rounded"
-                    />
-                </Stack>
+            {products?.products?.length === 0 && (
+                <Typography variant="h6" color="textSecondary" align="center">
+                    No hay productos disponibles
+                </Typography>
             )}
 
-            {/* Componente de carga */}
+            {/* Paginación */}
+            <Stack spacing={2} direction="row" justifyContent="center" padding={3}>
+                <Pagination                    
+                    count={pagination.totalPages}
+                    page={pagination.currentPage}
+                    onChange={handlePageChange}
+                    variant="outlined"
+                    shape="rounded"
+                    siblingCount={1} // Muestra 1 página antes y 1 después de la página actual
+                    boundaryCount={0} // Muestra 2 páginas al principio y 2 al final
+                />
+            </Stack>
+
+            {/* Cargando */}
             <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={loading}>
                 <CircularProgress color="inherit" />
             </Backdrop>
-
-
         </Box>
     );
 };
