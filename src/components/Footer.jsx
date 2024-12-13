ae8329cd2d8d2e536536179d6e7aa1f8bcc981d9
HitsoKeyco <olivosergio09@gmail.com>
2024-12-12 22:53:27 -0500

    - se ajusto el responsive de cart

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import './css/Footer.css';
 
 const Footer = () => {
@@ -18,33 +18,30 @@ const Footer = () => {
                 <div className='footer_section'>
                     <ul className='footer_info'>
                         <li className='footer_title'>Información</li>
-                        <li><a href="#" className='footer_link'>Acerca de nosotros 👷</a></li>
-                        <li><a href="#" className='footer_link'>Devoluciones 👷</a></li>
-                        <li><a href="#" className='footer_link'>Contactanos</a></li>
-                        <li><a href="#" className='footer_link'>Trabaja con nosotros 👷</a></li>
+                        <li><a href="#" className='footer_link' rel="noopener noreferrer">Acerca de nosotros 👷</a></li>
+                        <li><a href="#" className='footer_link' rel="noopener noreferrer">Devoluciones 👷</a></li>
+                        <li><a href="#" className='footer_link' rel="noopener noreferrer">Contactanos</a></li>
+                        <li><a href="#" className='footer_link' rel="noopener noreferrer">Trabaja con nosotros 👷</a></li>
                     </ul>
                 </div>
                 <div className='footer_section'>
                     <ul className='footer_info'>
                         <li className='footer_title'>Recursos</li>
-                        <li><a href="#" className='footer_link'>Blog 👷</a></li>
-                        <li><a href="#" className='footer_link'>Ayuda 👷</a></li>
+                        <li><a href="#" className='footer_link' rel="noopener noreferrer">Blog 👷</a></li>
+                        <li><a href="#" className='footer_link' rel="noopener noreferrer">Ayuda 👷</a></li>
                     </ul>
                 </div>
                 <div className='footer_section'>
                     <ul className='footer_social'>
                         <li className='footer_title'>Redes Sociales</li>
-                        <li><a href="#" className='footer_link'>Facebook</a></li>
-                        <li><a href="#" className='footer_link'>Twitter</a></li>
-                        <li><a href="#" className='footer_link'>Instagram</a></li>
+                        <li><a href="#" className='footer_link' target="_blank" rel="noopener noreferrer">Facebook</a></li>                        
+                        <li><a href="https://www.instagram.com/b.everchic/" className='footer_link' target="_blank" rel="noopener noreferrer">Instagram</a></li>
                     </ul>
                 </div>
             </div>
             <p className='footer_copyright_text'>&copy; 2024 Everchic. Todos los derechos reservados.</p>
 
         </footer>
-
-
     );
 };
 
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+
 import './css/ProductItem.css'
 import { useDispatch } from 'react-redux'
 import { deleteProduct, deleteProductFree, minusProduct, plusProduct } from '../store/slices/cart.slice'
+import PropTypes from 'prop-types';
 
 const ProductItem = ({ product, infoFree}) => {
     const dispatch = useDispatch();
@@ -22,43 +23,65 @@ const ProductItem = ({ product, infoFree}) => {
         dispatch(deleteProductFree({ productId: product.productId }))
     }
         
+    
     return (
         <>
             <div className='product_item_container'>
                 <div className="product_img_item_container">
-                    <img className='product_img_item' src={product.image?.url} alt="img_product" />
+                    <img className='product_img_item' src={product?.image?.url} alt="image" />
                 </div>
                 <div className="product_details_item_container">
-                    <h4 className='product_details_item_title'>Calcetines de {product.productName}</h4>
-                    <h5 className='product_details_item'>Stock: <span className='span_item'>{`${product.stock} Pares`} </span></h5>
-                    <h5 className='product_details_item'>
+                    <p className='product_details_item_title'>{product?.productName}</p>
+                    <p className='product_details_item'>Stock: <span className='span_item'>{`${product?.stock} Pares`} </span></p>
+                    <p className='product_details_item'>Talla: <span className='span_item'>{product?.size} </span></p>
+                    <p className='product_details_item'>
                         Subtotal Item: <span className='span_item'>
-                            {infoFree ? <span className='free_product_span'>Gratis</span> : <span>{`$${(product.quantity * product.priceUnit).toFixed(2)}`}</span>}
+                            {infoFree ? <span className='free_product_span'>Gratis</span> : <span className='span_item'>{`$${(product?.quantity * product?.priceUnit).toFixed(2)}`}</span>}
                         </span>
+
+                    </p>
+                    <p className='product_details_item'>
+                        {
+                            infoFree && <span>Cantidad: {product?.quantity}</span>
+                        }
                         {
                             infoFree && <i className='bx bxs-trash trash_free' onClick={handleRemoveProductFree}></i>
                         }
-                    </h5>
-                    <h5 className='product_details_item'>
-                        {
-                            infoFree && <span>Cantidad: {product.quantity}</span>
+                    </p>
 
-                        }
-                    </h5>
                     {!infoFree &&
                         <div className='product_details_handle'>
                             <i className='bx bx-minus product_cart_button_item_minus' onClick={handleMinusQuantity}></i>
-                            <span className='product_cart_text_pair_info'>{product.quantity} Pares</span>
+                            <span className='product_cart_text_pair_info'>{product?.quantity} Pares</span>
                             <i className='bx bx-plus product_cart_button_item_plus' onClick={handlePlusQuantity}></i>
                             <i className='bx bxs-trash' onClick={handleRemoveProduct}></i>
                         </div>
-                    }
-
-                    <h5 className='product_details_item_size'> Talla: <span className='span_item_size'>{product.size}</span></h5>
+                    }                    
                 </div>
             </div>
         </>
     )
 }
+
+
+
+ProductItem.propTypes = {
+    product: PropTypes.shape({
+        productId: PropTypes.string.isRequired,
+        productName: PropTypes.string.isRequired,
+        image: PropTypes.shape({
+            url: PropTypes.string
+        }),
+        stock: PropTypes.number.isRequired,
+        quantity: PropTypes.number.isRequired,
+        priceUnit: PropTypes.number.isRequired,
+        size: PropTypes.string.isRequired
+    }).isRequired,
+    infoFree: PropTypes.bool
+};
+
+ProductItem.defaultProps = {
+    infoFree: false
+};
 
 export default ProductItem;
